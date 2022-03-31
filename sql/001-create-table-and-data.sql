6d0609c23644f226c4c5ea686fa5d813aa8afdab
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-03-31 20:31:12 +0900

    SQL文にフォーマットを適用 AUTO_INCREMENT=1を削除

--- sql/001-create-table-and-data.sql
+++ sql/001-create-table-and-data.sql
@@ -6,8 +6,7 @@ CREATE TABLE datecalc (
     plusyear INT(50),
     plusmonth INT(50),
     plusday INT(50)
-    )
-    AUTO_INCREMENT = 1;
+);
 
 INSERT IGNORE INTO datecalc (id, name, plusyear, plusmonth, plusday) VALUES (1, 'A', 1, 1, 1);
 INSERT IGNORE INTO datecalc (id, name, plusyear, plusmonth, plusday) VALUES (2, 'B', 2, 2, 2);
