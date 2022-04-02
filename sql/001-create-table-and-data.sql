52e61526d382cd226a9b42d1321b78c4ce6c62a6
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-04-02 11:34:17 +0900

    末尾に改行コードを挿入

--- sql/001-create-table-and-data.sql
+++ sql/001-create-table-and-data.sql
@@ -11,3 +11,4 @@ CREATE TABLE datecalc (
 INSERT IGNORE INTO datecalc (id, name, plusyear, plusmonth, plusday) VALUES (1, 'A', 1, 1, 1);
 INSERT IGNORE INTO datecalc (id, name, plusyear, plusmonth, plusday) VALUES (2, 'B', 2, 2, 2);
 INSERT IGNORE INTO datecalc (id, name, plusyear, plusmonth, plusday) VALUES (3, 'C', 3, 3, 3);
+
